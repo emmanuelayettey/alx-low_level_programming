34f06f83f3f49697108be1dab413b8ba38ac7f57
emmanuelayettey <emmanuelayettey029@gmail.com>
2023-10-05 09:59:43 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - A program that prints; Programming is like building a multilingual puzzle
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
