4a67a5347b77bfb25ea9489fcf7f390e38e07435
emmanuelayettey <emmanuelayettey029@gmail.com>
2023-10-11 07:56:33 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - a program that causes an infinite loop
+* main - causes an infinite loop
 * Return: 0
 */
 int main(void)
@@ -15,3 +15,4 @@ i = 0;
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
+
