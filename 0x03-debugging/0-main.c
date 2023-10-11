455801ed1a47ee72972fc148dde4ec2ca8337a1d
emmanuelayettey <emmanuelayettey029@gmail.com>
2023-10-11 08:12:46 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-* main - a program that tests function that prints if integer is 
-* positive or negative
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
